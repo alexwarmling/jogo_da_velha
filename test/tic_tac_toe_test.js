67bea69c9c43196da288ce5deea20a8c63d8ac0c
Stefan Teixeira <stefanfk@gmail.com>
2015-03-30 19:01:46 -0300

    Including more tests

--- test/tic_tac_toe_test.js
+++ test/tic_tac_toe_test.js
@@ -1,10 +1,39 @@
 describe('Tic-tac-toe Test', function() {
+  var sandbox;
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
 
   it('should call the playAgain function successfully', function() {
-    sinon.stub(document, 'getElementById').returns('');
+    sandbox.stub(document, 'getElementById').returns('');
     playAgain();
-    
+
     expect(playAgain).toBeDefined();
+  });
+
+  it('should get the correct winner, when it is X', function() {
+    sandbox.stub(document, 'getElementById').returns('<td>X</td>')
+    spyOn(window, 'alert');
+
+    getWinner();
+    expect(window.alert).toHaveBeenCalledWith('bla');
+  });
+
+  it('should get the correct winner, when it is O', function() {
+    sandbox.stub(document, 'getElementById').returns('<td>O</td>')
+    spyOn(window, 'alert');
+
+    getWinner();
+    expect(window.alert).toHaveBeenCalledWith('bla');
+  });
+
+  it('should display an adequate message when there is no winner', function() {
+
   });
 
 });
